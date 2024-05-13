950942be315947ec5932e89085985fa1ab61fbee
EddyDiazS <eddya.diazs@konradlorenz.edu.co>
2024-05-13 14:41:45 -0500

    Estudiante.js
    Creación de la clase Estudiante.js, sus constantes, y el método POST para la creación de cada uno.

---
+++ src/routes/Estudiante.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const clasesSchema = require("../models/EstudianteD");
+const conduccionSchema = require("../models/EstudianteD");
+const verifyToken = require('../routes/validate_token');
+const EstudianteD = require("../models/EstudianteD");
+
+//Nuevo estudiante
+router.post("/EstudianteD", verifyToken, (req, res) => {
+    const clases = clasesSchema(req.body);
+    EstudianteD
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
