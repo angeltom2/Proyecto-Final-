9a5f0862de00fae621898ec24f6d591e3b4e8f17
Miguel Luis <miguelap3104@gmail.com>
2024-05-13 14:33:33 -0500

    autenticathionP.js
    Se crea el Login para el profesor.

--- src/routes/autenticathionP.js
+++ src/routes/autenticathionP.js
@@ -23,3 +23,25 @@ router.post("/signup", async (req, res) => {
         token,
     });
 });
+
+//inicio de sesión
+router.post("/login", async (req, res) => {
+    // validaciones
+    const { error } = userSchema.validate(req.body.correo, req.body.clave);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    //Buscando el usuario por su dirección de correo
+    const profesor = await userSchema.findOne({ correo: req.body.correo });
+    //validando si no se encuentra
+    if (!profesor) return res.status(400).json({ error: "Usuario no encontrado" });
+    //Transformando la contraseña a su valor original para 
+    //compararla con la clave que se ingresa en el inicio de sesión
+    const validPassword = await bcrypt.compare(req.body.clave, user.clave);
+    if (!validPassword)
+        return res.status(400).json({ error: "Clave no válida" });
+    res.json({
+        error: null,
+        data: "Bienvenido(a)",
+    });
+});
+
+module.exports = router;
