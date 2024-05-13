583508bbe07bb10b2fae98a389665b034e4850b6
Oublack <jsebasrg@gmail.com>
2024-05-13 15:44:46 -0500

    notas.js
    se coloco el metodo eliminar

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -30,11 +30,25 @@ router.get("/notas/:id", (req, res) => {
 //Modificar las notas  por el id
 router.put("/notas/:id", (req, res) => {
     const { id } = req.params;
-    const { estudiante,clase,tipo,categoria,puntaje,observaciones} = req.body;
+    const { estudiante, clase, tipo, categoria, puntaje, observaciones } = req.body;
     clasesSchema
         .updateOne({ _id: id }, {
-            $set: {  estudiante,clase,tipo,categoria,puntaje,observaciones}
+            $set: { estudiante, clase, tipo, categoria, puntaje, observaciones }
         })
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Eliminar las notas por el id 
+router.delete("/notas/:id", (req, res) => {
+    const { id } = req.params;
+    clasesSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+module.exports = router;
