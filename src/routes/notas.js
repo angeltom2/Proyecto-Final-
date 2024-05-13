28449f0f3660c5eae8e1ca6c18e33df2b449745c
Oublack <jsebasrg@gmail.com>
2024-05-13 15:42:29 -0500

    notas.js
    se coloco metodo get por id

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -19,3 +19,11 @@ router.get("/notas", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Consultar las notas por el id
+router.get("/notas/:id", (req, res) => {
+    const { id } = req.params;
+    conduccionSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
